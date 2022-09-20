83a6a6c7fd99b9052364f45989a8f6c263b5a6b3
Marley Watson <marleykw17@gmail.com>
2022-09-20 17:16:22 -0400

    commit tree 566680391888e0a69610708a73cb81e35c960e29

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function handleBlur(event) {
+        event.preventDefault();
+        console.log("Hey! Eyes on me!");
+      }
+    function handleFocus(event) {
+        event.preventDefault();
+        console.log("Good!");
+      }
+    return (
+        <div>
+            <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me.</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event) {
+        event.preventDefault();
+        console.log("Entering password...");
+      }
     return (
-        <div></div>
+        <div>
+            <form onChange={handleChange}>
+                <input type="password" name="password" placeholder="Enter password..." />
+            </form>
+        </div>
     )
 }
 
